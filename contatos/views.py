7b783418214443be5bc21d903170c735ba75c6fa
Glfx20 <jsgleisson@gmail.com>
2023-04-23 16:56:28 -0300

    Melhorias
    Novas atualização na tabela
     - criação de filtros
     - criação de paginação

--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.http import Http404
 from .models import Contato
+from django.db.models import Q, Value
+from django.db.models.functions import  Concat
 
 # Create your views here.
 def index(request):
@@ -25,3 +27,30 @@ def ver_contato(request, contato_id):
         raise Http404()
 
     return render(request, 'contatos/ver_contato.html', {  'contato' : contato })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None:
+        raise  Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')#Valeu vai simular um campo vazio
+
+    #para utilizar busca considerando duas colunas separadas
+    contatos = Contato.objects.annotate(nome_completo = campos).filter(
+       Q(nome_completo__icontains=termo)  | Q(telefone__icontains=termo)
+    )
+
+    # #Pode usar o Filtro ao invez de fazer o If no Html index  / Criando o filtro de itens Q = OR
+    # contatos = Contato.objects.order_by('-id').filter(
+    #     Q(nome__icontains=termo) | Q(sobrenome__icontains=termo),
+    #     mostrar=True
+    # )
+
+    #Configurção de paginação.
+    paginator = Paginator(contatos, 5)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {  'contatos' : contatos  })
