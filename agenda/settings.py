7c7edd10d47b1f41f79e22ac6f06ba544e20824d
Gleisson Santos <jsgleisson@gmail.com>
2023-04-24 21:17:46 -0300

    v10
    Criação de ambiente de cadastro de usuario

--- agenda/settings.py
+++ agenda/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contatos.apps.ContatosConfig',
+    'accounts.apps.AccountsConfig',
+
 ]
 
 MIDDLEWARE = [
